34bdde8635845abd227ce43962f36528ec746885
MyVo <my.vo@propzy.com>
2022-08-25 12:20:55 +0700

    update

--- TekoProduct/UI/ProductViewController/ProductViewController.swift
+++ TekoProduct/UI/ProductViewController/ProductViewController.swift
@@ -6,22 +6,69 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SVPullToRefresh
 
 class ProductViewController: UIViewController {
     @IBOutlet weak var tblContent: UITableView!
-    var products: [Product] = []
+    let viewModel: ProductViewModel = ProductViewModel()
+    private let disposeBag = DisposeBag()
+    let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        tblContent.addSubview(refreshControl)
+        
+        tblContent.addInfiniteScrolling { [weak self] in
+            guard let self = self else { return }
+        }
+        viewModel.getProducts()
+        
+        bindViewModel()
     }
     
+    @objc private func refreshData() {
+        viewModel.getProducts()
+    }
     
+    private func bindViewModel() {
+        viewModel.output.getProducts
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] isShowsInfiniteScrolling in
+                guard let self = self else { return }
+                self.refreshControl.endRefreshing()
+                self.tblContent.infiniteScrollingView.stopAnimating()
+                self.tblContent.reloadData()
+                self.tblContent.showsInfiniteScrolling = isShowsInfiniteScrolling
+            }).disposed(by: disposeBag)
+        
+        viewModel.output.responseError
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] event in
+                guard let self = self,
+                      let error = event.element else { return }
+                self.showAlert(title: error.error, message: error.data?.data)
+            }.disposed(by: disposeBag)
+        
+        viewModel.output.isLoading
+            .observe(on: MainScheduler.instance)
+            .subscribe { event in
+                guard let loading = event.element else { return }
+                if loading {
+                    Util.showLoading()
+                } else {
+                    Util.hideLoading()
+                }
+            }.disposed(by: disposeBag)
+    }
 }
 
 extension ProductViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return products.count
+        return viewModel.products.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
